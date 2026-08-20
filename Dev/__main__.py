002f2932503e41cab669adf795b115a33bf34e89
Toxic <dev@toxics.me>
2026-08-21 01:35:43 +0530

    Update __main__.py

--- Dev/__main__.py
+++ Dev/__main__.py
@@ -17,10 +17,6 @@ async def main():
     for module in all_modules:
         importlib.import_module(f"Dev.plugins.{module}")
     logger.info(f"Loaded {len(all_modules)} modules.")
-
-  
-    if config.COOKIES_URL:
-        await yt.save_cookies(config.COOKIES_URL)
       
     sudoers = await db.get_sudoers()
     app.sudoers.update(sudoers)
